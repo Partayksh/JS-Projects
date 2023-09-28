ed2ae02e09ebcb73aa311e1d2a7c7bf0ece653b1
Parveen Tyagi <118175481+Partayksh@users.noreply.github.com>
2023-09-28 13:48:54 +0530

    Added file

---
+++ Digital-clock/script.js
@@ -0,0 +1,22 @@
+let hr = document.getElementById('hour');
+let mn = document.getElementById('min');
+let sc = document.getElementById('sec');
+
+function showTime(){
+    let date = new Date();
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+   
+    
+    let hRotation  = 30 * hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    mn.style.transform =`rotate(${mRotation}deg)`; 
+    sc.style.transform =`rotate(${sRotation}deg)`; 
+
+}
+setInterval(showTime, 1000);
